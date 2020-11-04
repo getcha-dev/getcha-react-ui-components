a31342afcbd41d9845031c86a54767b0353c2f76
cdbrouk <cdbrouk@gmail.com>
2020-11-04 10:57:26 +0900

    Initialize story book

---
+++ src/components/svg/common/UserSVG.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../../../static/palette';
+import SVGIcon from '../SVGIcon';
+
+export interface UserSVGProps {
+  width?: number;
+  height?: number;
+  fill?: string;
+}
+
+const SVG = styled(SVGIcon)<UserSVGProps>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+`;
+
+export const UserSVG: React.FC<UserSVGProps> = ({
+  width = 28,
+  height = 28,
+  fill = palette.base.black,
+}) => (
+  <SVG viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
+    <path
+      d="M21.9561 23H6.03702C5.69572 23 5.43217 22.71 5.47473 22.3815L5.69449 20.7004C6.22444 16.6469 9.78147 13.6083 13.9965 13.6083C18.2116 13.6083 21.7686 16.6469 22.2986 20.7004L22.5184 22.3815C22.5613 22.71 22.2974 23 21.9561 23Z"
+      fill={fill}
+    />
+    <path
+      d="M13.9967 12.6586C16.1784 12.6586 17.947 10.9441 17.947 8.82929C17.947 6.71443 16.1784 5 13.9967 5C11.815 5 10.0464 6.71443 10.0464 8.82929C10.0464 10.9441 11.815 12.6586 13.9967 12.6586Z"
+      fill={fill}
+    />
+  </SVG>
+);
