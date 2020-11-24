711cf6d00352da0724ff7a3c903e29f12d15dc2b
cdbrouk <cdbrouk@gmail.com>
2020-11-24 13:37:24 +0900

    Change GNB type def

--- src/components/common/GNB.tsx
+++ src/components/common/GNB.tsx
@@ -196,12 +196,12 @@ const EmptyBlock = styled.div`
   height: 28px;
 `;
 
-const GNB: React.FC<GNBStyleProps & GNBDataProps> = ({
+const GNB = ({
   darkMode = false,
   fixed = true,
   showTitle = true,
   title = 'Please set page title',
-}) => {
+}: GNBStyleProps & GNBDataProps): JSX.Element => {
   const [isMainActive, setIsMainActive] = useState(true);
 
   return (
